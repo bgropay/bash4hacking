e156c168edf07e4084e6338b73b9f98f278b0b72
bgropay <bgropay@gmail.com>
2024-08-05 01:22:10 +0700

    Update install_wpscry.sh

--- installer/install_wpscry.sh
+++ installer/install_wpscry.sh
@@ -7,22 +7,50 @@
 
 url_reaver="https://github.com/t6x/reaver-wps-fork-t6x"
 url_pixiewps="https://github.com/wiire-a/pixiewps"
+url_aircrack="https://github.com/aircrack-ng/aircrack-ng"
 path_app="/opt"
 path_reaver="reaver-wps-fork-t6x"
 path_pixiewps="pixiewps"
+path_aircrack="aircrack-ng"
 
 daftar_dependensi_reaver=(
         "wireless-tools"
         "build-essential"
         "libpcap-dev"
 	# "pixiewps"
-	"aircrack-ng"
+	# "aircrack-ng"
 	"libsdl2-2.0-0"
 )
 
 # daftar_dependensi_pixiewps=(
 #         "build-essential"
 # )
+
+daftar_dependensi_aircrack=(
+        # "build-essential" 
+	"autoconf"
+        "automake"
+	"libtool"
+        "pkg-config"
+	"libnl-3-dev"
+        "libnl-genl-3-dev"
+	"libssl-dev ethtool"
+        "shtool"
+	"rfkill"
+        "zlib1g-dev"
+	# "libpcap-dev"
+        "libsqlite3-dev"
+	"libpcre2-dev"
+        "libhwloc-dev"
+	"libcmocka-dev"
+        "hostapd"
+	"wpasupplicant"
+        "tcpdump"
+	"screen"
+        "iw"
+	"usbutils"
+        "expect"
+)
 
 apt-get update -y
 
@@ -36,6 +64,11 @@ done
 #        apt-get install "${dependensi_pixiewps}"
 # done
 
+# menginstal seluruh dependensi yang diperlukan oleh aircrack-ng
+for dependensi_aircrack in "${daftar_dependensi_aircrack[@]}"; do
+	apt-get install "${dependensi_aircrack}" -y
+done
+
 cd "${path_app}"
 git clone "${url_reaver}"
 cd "${path_reaver}/src"
@@ -47,3 +80,11 @@ git clone "${url_pixiewps}"
 cd "${path_pixiewps}"
 make
 make install
+cd ../ # kembali ke direktori '/opt'
+git clone "${url_aircrack}"
+cd "${path_aircrack}"
+autoreconf -i
+./configure --with-experimental
+make
+make install 
+ldconfig
