110eefa34181f6a286a0c1692b150e25516dd4a5
bgropay <bgropay@gmail.com>
2024-08-09 21:32:32 +0700

    Update instal_wpscry.sh

--- installer/instal_wpscry.sh
+++ installer/instal_wpscry.sh
@@ -5,19 +5,20 @@
 # Jika ada bug atau masalah saat proses instalasi
 # laporkan di 'https://github.com/bgropay/bash4hacking/issues'
 
+# Fungsi untuk mengecek apakah Anda memiliki koneksi internet atau tidak.
 function cek_koneksi_internet(){
         echo "[*] Mengecek koneksi internet..."
-	
         if ping -c 1 8.8.8.8 >> /dev/null 2>&1; then
 	        sleep 3
                 echo "[+] Anda memiliki koneksi internet. Proses instalasi dapat dilanjutkan."
 	else
                 sleep 5
-                echo "[-] Anda tidak memiliki koneksi internet. Pastikan Anda memiliki koneksi internet untuk menginstal wpscry."
+                echo "[-] Anda tidak memiliki koneksi internet. Proses instalasi tidak dapat dilanjutkan."
 		exit 1
         fi
 }
 
+# Fungsi untuk mengecek apakah Anda sudah menginstal git atau belum.
 function cek_git(){
         echo "[*] Mengecek git..."
 	if command -v "git" >> /dev/null 2>&1; then
@@ -25,108 +26,150 @@ function cek_git(){
                 echo "[+] Git sudah terinstal."
 	else
                 sleep 5
-		echo "[-] Git belum terinstal. Pastikan git sudah terinstal untuk menginstal wpscry."
+		echo "[-] Git belum terinstal."
+                echo "[*] Menginstal git..."
+		sleep 3
+                apt-get install "git" -y
+		if [[ $? -eq 0 ]]; then
+                        echo "[+] Git berhasil diinstal. proses instalasi dapat dilanjutkan."
+		else
+                        echo "[-] Git gagal diinstal. proses instalasi tidak dapat dilanjutkan."
+			exit 1
                 exit 1
 	fi
 }
 
-# url tools
-url_reaver="https://github.com/t6x/reaver-wps-fork-t6x"
-url_pixiewps="https://github.com/wiire-a/pixiewps"
-url_aircrack="https://github.com/aircrack-ng/aircrack-ng"
-
-# path
-path_app="/opt"
-path_reaver="reaver-wps-fork-t6x"
-path_pixiewps="pixiewps"
-path_aircrack="aircrack-ng"
-
-# list dependensi yang diperlukan oleh reaver.
-daftar_dependensi_reaver=(
-        "wireless-tools"
-        "build-essential"
-        "libpcap-dev"
-	# "pixiewps"
-	# "aircrack-ng"
-	"libsdl2-2.0-0"
-)
-
-# List dependensi yang diperlukan oleh pixiewps.
-# daftar_dependensi_pixiewps=(
-#         "build-essential"
-# )
-
-# List dependensi yang diperlukan oleh aircrack-ng.
-daftar_dependensi_aircrack=(
-        # "build-essential" 
-	"autoconf"
-        "automake"
-	"libtool"
-        "pkg-config"
-	"libnl-3-dev"
-        "libnl-genl-3-dev"
-	"libssl-dev ethtool"
-        "libssl-dev"
-        "shtool"
-	"rfkill"
-        "zlib1g-dev"
-	# "libpcap-dev"
-        "libsqlite3-dev"
-	"libpcre2-dev"
-        "libhwloc-dev"
-	"libcmocka-dev"
-        "hostapd"
-	"wpasupplicant"
-        "tcpdump"
-	"screen"
-        "iw"
-	"usbutils"
-        "expect"
-)
-
-# Memperbaharui repositori linux.
-apt-get update -y
-
-# Menginstal seluruh dependensi yang diperlukan oleh reaver 
-for dependensi_reaver in "${daftar_dependensi_reaver[@]}"; do
-	apt-get install "${dependensi_reaver}" -y
-done
-
-# Menginstal seluruh dependensi yang diperlukan oleh pixiewps 
-# for dependensi_pixiewps in "${daftar_dependensi_pixiewps[@]}"; do
-#        apt-get install "${dependensi_pixiewps}"
-# done
-
-# Menginstal seluruh dependensi yang diperlukan oleh aircrack-ng
-for dependensi_aircrack in "${daftar_dependensi_aircrack[@]}"; do
-	apt-get install "${dependensi_aircrack}" -y
-done
-
 # Pindah ke folder '/opt'
 cd "${path_app}"
 
-# Instal reaver.
-git clone "${url_reaver}"
-cd "${path_reaver}/src"
-./configure
-make
-make install
+# Fungsi untuk menginstal tools reaver.
+function instal_reaver(){
+        url_reaver="https://github.com/t6x/reaver-wps-fork-t6x"
+        path_reaver="reaver-wps-fork-t6x"
 
-cd ../../ # kembali ke direktori '/opt'
+        # list dependensi yang diperlukan oleh reaver.
+        daftar_dependensi_reaver=(
+                "wireless-tools"
+                "build-essential"
+                "libpcap-dev"
+        	# "pixiewps"
+	        # "aircrack-ng"
+	        "libsdl2-2.0-0"
+        )
 
-# Instal pixiewps
-git clone "${url_pixiewps}"
-cd "${path_pixiewps}"
-make
-make install
+        # Menginstal seluruh dependensi yang diperlukan oleh reaver 
+        for dependensi_reaver in "${daftar_dependensi_reaver[@]}"; do
+	        apt-get install "${dependensi_reaver}" -y
+        done
 
-cd ../ # kembali ke direktori '/opt'
+        # Instal reaver.
+        git clone "${url_reaver}"
+        cd "${path_reaver}/src"
+        ./configure
+        make
+        make install
+	
+	cd ../../ # kembali ke direktori '/opt'
+}
 
-# Instal aircrack-ng
-git clone "${url_aircrack}"
-cd "${path_aircrack}"
-autoreconf -i
-./configure --with-experimental
-make
-make install 
-ldconfig
+function instal_pixiewps(){
+        url_pixiewps="https://github.com/wiire-a/pixiewps"
+        path_pixiewps="pixiewps"
+
+        # List dependensi yang diperlukan oleh pixiewps.
+        # daftar_dependensi_pixiewps=(
+        #         "build-essential"
+        # )
+
+        # Menginstal seluruh dependensi yang diperlukan oleh pixiewps 
+        # for dependensi_pixiewps in "${daftar_dependensi_pixiewps[@]}"; do
+        #        apt-get install "${dependensi_pixiewps}"
+        # done
+
+        # Instal pixiewps
+        git clone "${url_pixiewps}"
+        cd "${path_pixiewps}"
+        make
+        make install
+
+        cd ../ # kembali ke direktori '/opt'
+}
+
+function instal_aircrack(){
+        url_aircrack="https://github.com/aircrack-ng/aircrack-ng"
+        path_aircrack="aircrack-ng"
+        
+        # List dependensi yang diperlukan oleh aircrack-ng.
+        daftar_dependensi_aircrack=(
+                # "build-essential" 
+        	"autoconf"
+                "automake"
+        	"libtool"
+                "pkg-config"
+        	"libnl-3-dev"
+                "libnl-genl-3-dev"
+        	"libssl-dev ethtool"
+                "libssl-dev"
+                "shtool"
+        	"rfkill"
+                "zlib1g-dev"
+        	# "libpcap-dev"
+                "libsqlite3-dev"
+        	"libpcre2-dev"
+                "libhwloc-dev"
+        	"libcmocka-dev"
+                "hostapd"
+        	"wpasupplicant"
+                "tcpdump"
+        	"screen"
+                "iw"
+        	"usbutils"
+                "expect"
+       )
+
+        # Menginstal seluruh dependensi yang diperlukan oleh aircrack-ng
+        for dependensi_aircrack in "${daftar_dependensi_aircrack[@]}"; do
+                apt-get install "${dependensi_aircrack}" -y
+        done
+
+        # Instal aircrack-ng
+        git clone "${url_aircrack}"
+        cd "${path_aircrack}"
+        autoreconf -i
+        ./configure --with-experimental
+        make
+        make install 
+        ldconfig
+
+        cd ../ # kembali ke direktori '/opt'
+}
+
+
+
+
+
+# path
+path_app="/opt"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
